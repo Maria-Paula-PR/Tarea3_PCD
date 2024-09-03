672ea03f326fcb017968ec570fd93161873cb9a2
mppr <perezromomariapaula@gmail.com>
2024-09-03 10:48:06 -0600

    commit listas recommendations

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+import json
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 import uvicorn
-import json
 from typing import List
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-# Validar la conexión con la base de datos
 def get_db():
     try:
         db = SessionLocal()
@@ -19,37 +18,36 @@ def get_db():
     finally:
         db.close()
 
-# entrada del usuario
 class UserCreate(BaseModel):
     user_name: str = Field(min_length=1)
     user_email: str = Field(min_length=1)
-    age: int = Field(gt=0, lt=110, default=None)
-    recommendations:  List[str] = [] 
-    zip_code: str = Field(min_length=4, max_length=8, default=None)
+    age: int = Field(gt=0, lt=110)
+    recommendations: List[str] = []
+    zip_code: str = Field(min_length=4, max_length=8) 
 
 @app.get("/")
 def read_api(db: Session = Depends(get_db)):
-    return db.query(models.User).all()
+    users = db.query(models.User).all()
+    for user in users:
+        user.recommendations = json.loads(user.recommendations) if user.recommendations else []
+    return users
 
 @app.post("/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # Revisar si el correo ya está registrado
     db_user = db.query(models.User).filter(models.User.user_email == user.user_email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    user_model = models.User(
-        user_name=user.user_name,
-        user_email=user.user_email,
-        age=user.age,
-        zip_code=user.zip_code
-    )
-    #user_model.set_recommendations(user.recommendations)  # Serializar recomendaciones
+    user_model = models.User()
+    user_model.user_name = user.user_name
+    user_model.user_email = user.user_email
+    user_model.age = user.age
+    # convertir la lista a una cadena jsons antes de guardarla
+    user_model.recommendations = json.dumps(user.recommendations)
+    user_model.zip_code = user.zip_code
 
-    # Agregar el usuario a la db 
     db.add(user_model)
     db.commit()
-    db.refresh(user_model)
 
     return user_model
 
@@ -58,19 +56,17 @@ def update_user(user_id: int, user: UserCreate, db: Session = Depends(get_db)):
     user_model = db.query(models.User).filter(models.User.user_id == user_id).first()
 
     if user_model is None:
-        raise HTTPException(
-            status_code=404,
-            detail=f"ID {user_id} : Does not exist"
-        )
+        raise HTTPException(status_code=404, detail=f"ID {user_id} does not exist")
 
     user_model.user_name = user.user_name
     user_model.user_email = user.user_email
     user_model.age = user.age
-    user_model.set_recommendations(user.recommendations)
+    # convertir la lista a una cadena jsons antes de guardarla
+    user_model.recommendations = json.dumps(user.recommendations)
     user_model.zip_code = user.zip_code
 
+    db.add(user_model)
     db.commit()
-    db.refresh(user_model)
 
     return user_model
 
